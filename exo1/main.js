d236267eb44ba00eff35c31a03986fa47a3a369c
Vingt-six <devloppementmarouane@gmail.com>
2021-12-14 15:09:29 +0100

    Exo fonction

---
+++ exo1/main.js
@@ -0,0 +1,73 @@
+// EXO 1
+// function addition(nbr1, nbr2) {
+//     console.log(nbr1 + nbr2);
+// }
+// addition(7, 7);
+
+
+// EXO 2
+// function soustraction(nbr1, nbr2) {
+//     console.log(nbr1 - nbr2);
+// }
+// soustraction(50, 40);
+
+
+// EXO 3
+// function multi(nbr1, nbr2) {
+//     console.log(nbr1 * nbr2);
+// }
+// multi(3, 3);
+
+
+// EXO 4
+// function div(nbr1, nbr2) {
+//     console.log(nbr1 / nbr2);
+// }
+// div(81, 9);
+
+
+// EXO 5
+// function reste(nbr1, nbr2) {
+//     return(nbr1 % nbr2);
+// }
+// console.log(reste(8, 3)); ;
+
+
+// EXO 6
+// function carre(nbr1) {
+//     return(Math.sqrt(nbr1));
+// }
+// console.log(carre(81));
+
+
+// EXO 7
+// function exposant(nbr1, nbr2) {
+//     return(Math.pow(nbr1, nbr2));
+// }
+// console.log(exposant(9, 2));
+
+
+// EXO 8
+// function capital(mot) {
+//         mot = mot.charAt(0).toUpperCase() + mot.slice(1).toLowerCase();
+//         return(`tu as dis ${mot}`);
+// }
+// console.log(capital("majuScUle"));
+
+
+// EXO 9
+// function calcul(nbr1, operateur, nbr2) {
+//     switch (operateur) {
+//         case "+":
+//             return nbr1 + nbr2;
+//         case "-":
+//             return nbr1 - nbr2;
+//         case "*":
+//             return nbr1 * nbr2;
+//         case "/":
+//             return nbr1 / nbr2;
+//         default:
+//             return("pour effectuer le calcul, mettez un operateur valide");
+//     }
+// }
+// console.log(calcul(7, "*", 9));
---
+++ exo2/main.js
@@ -0,0 +1,35 @@
+// EXO 1
+// function rever(nbr1, nbr2, nbr3) {
+//     let tab = [nbr1, nbr2, nbr3];
+//     tab.reverse();
+//     console.log(tab.join(""));
+// }
+// rever(1,2,3);
+
+
+// EXO 2
+// function divPar2(nbr1) {
+//     switch (nbr1%2) {
+//         case 0:
+//             console.log(`Le nombre ${nbr1} est divisible par 2 qui est égale à ${nbr1 / 2}`);
+//             break;
+//         case 1:
+//             console.log("Ce nombre n'est pas divisible par 2");
+//             break;
+//         default:
+//             console.log("Ce n'est pas un nombre");
+//             break;
+//     }
+// }
+// divPar2(556);
+
+
+// EXO 3
+// function login(quest1, rep1) {
+//     let mdp = prompt("Entrez votre mot de passe")
+//     while (mdp != "mdp") {
+//         mdp = prompt("Biiiip mauvais")
+//     }
+//     alert(`Authentification réussi`);
+// }
+// login();
